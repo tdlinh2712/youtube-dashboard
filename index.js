94ee68ee0be6aff5c1225a5785ba83f2be687987
tdlinh2712 <tdlinh2712@gmail.com>
2020-11-07 15:17:09 +1100

    add routing in production

--- index.js
+++ index.js
@@ -30,6 +30,19 @@ app.use(passport.session());
 require('./routes/authRoutes.js')(app, oauth2Client);
 require('./routes/youtubeRoutes.js')(app,oauth2Client);
 
+//routing in production
+if(process.env.NODE_ENV=== 'production') {
+    //express will serve up production assets like our main.js or main.css files
+    app.use(express.static('client/build')); //if express doesnt recognize the route, try to look into client/build to see if there's anything that matches
+
+    //express will serve up index.html file if it doesnt recognize the route
+    //for if there is nothing in routes folder and client/build => give index.html
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+    })
+}
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
